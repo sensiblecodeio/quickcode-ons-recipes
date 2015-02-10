0ff39218f2036929f041b1bb068e92fb8ab183fd
Dragon Dave McKee <dragon@scraperwiki.com>
2015-02-10 12:03:18 +0000

    test

--- test/test.py
+++ test/test.py
@@ -4,8 +4,7 @@ def per_file(tabs):
     return "*"
 
 def per_tab(tab):
-    print tab.excel_ref("B2").value
-    print tab.excel_ref("B2").fill(RIGHT)
-    print tab.excel_ref("B2").fill(DOWN)
-    return []
+    tab.set_header("kitten", "kitten")
+    tab.excel_ref("A1").fill(RIGHT).is_header('top', UP, strict=True)
+    return tab.excel_ref("B2").fill(RIGHT).fill(DOWN)
 
