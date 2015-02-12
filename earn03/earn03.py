aa41ef760e35218dc101a59d20ba0d6695857ddb
Dragon Dave McKee <dragon@scraperwiki.com>
2015-02-12 16:21:08 +0000

    bodgefix not scanning blank cells

--- earn03/earn03.py
+++ earn03/earn03.py
@@ -5,9 +5,9 @@ def per_file(tableset):
 
 def per_tab(tab):
     anchor=tab.filter("CDID").assert_one()
-    obs = anchor.shift(DOWN).shift(RIGHT).fill(RIGHT).fill(DOWN)
+    obs = anchor.shift(DOWN).shift(RIGHT).fill(RIGHT).is_not_blank.fill(DOWN).is_not_blank
 
-    tab.col('A').is_header(TIME,LEFT, strict=True)
+    tab.col('A').is_not_blank.is_header(TIME,LEFT, strict=True)
     tab.filter("Agriculture, Forestry and Fishing").fill(LEFT).fill(RIGHT).is_not_blank.is_header('Broad Industry Group', LEFT)
     anchor.shift(RIGHT).shift(RIGHT).shift(UP).fill(LEFT).fill(RIGHT).is_not_blank.is_header('AWE Breakdown', UP, strict=True)
 
