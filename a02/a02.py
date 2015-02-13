6ac6cd0def17a05ee0844ddda5962ac8a0abede3
Dragon Dave McKee <dragon@scraperwiki.com>
2015-02-13 15:49:19 +0000

    no regex in a02 for date: is_date()

--- a02/a02.py
+++ a02/a02.py
@@ -7,9 +7,9 @@ def per_tab(tab):
     obs = tab.filter("MGSL").assert_one().shift(DOWN).fill(RIGHT).fill(DOWN).filter(is_number).is_not_italic
 
     tab.col('A').one_of(['Male', 'Female', 'All Persons']).dimension('gender', CLOSEST, ABOVE)
-    tab.col('A').regex("...-... (?:19|20)\d\d").dimension(TIME, DIRECTLY, LEFT)
+    tab.col('A').is_date().dimension(TIME, DIRECTLY, LEFT)
     tab.regex("All aged .*").dimension('ages', CLOSEST, UP)
     tab.filter("Total economically active").fill(LEFT).fill(RIGHT).is_not_blank.dimension('indicator', DIRECTLY, ABOVE)
 
-    tab.set_header('adjusted_yn', tab.name)
+    tab.dimension('adjusted_yn', tab.name)
     return obs
