194881dd08823429fdc14b0b613b8b6d428a9c7d
Dragon Dave McKee <dragon@scraperwiki.com>
2015-02-16 15:13:11 +0000

    is_not_function()

--- a02/a02.py
+++ a02/a02.py
@@ -4,12 +4,12 @@ def per_file(tableset):
     return "*"
 
 def per_tab(tab):
-    obs = tab.filter("MGSL").assert_one().shift(DOWN).fill(RIGHT).fill(DOWN).is_number().is_not_italic
+    obs = tab.filter("MGSL").assert_one().shift(DOWN).fill(RIGHT).fill(DOWN).is_number().is_not_italic()
 
     tab.col('A').one_of(['Male', 'Female', 'All Persons']).dimension('gender', CLOSEST, ABOVE)
     tab.col('A').is_date().dimension(TIME, DIRECTLY, LEFT)
     tab.regex("All aged .*").dimension('ages', CLOSEST, UP)
-    tab.filter("Total economically active").fill(LEFT).fill(RIGHT).is_not_blank.dimension('indicator', DIRECTLY, ABOVE)
+    tab.filter("Total economically active").fill(LEFT).fill(RIGHT).is_not_blank().dimension('indicator', DIRECTLY, ABOVE)
 
     tab.dimension('adjusted_yn', tab.name)
     return obs
--- earn03/earn03.py
+++ earn03/earn03.py
@@ -5,9 +5,9 @@ def per_file(tableset):
 
 def per_tab(tab):
     anchor=tab.filter("CDID").assert_one()
-    obs = anchor.shift(DOWN).shift(RIGHT).fill(RIGHT).is_not_blank.fill(DOWN).is_not_blank
+    obs = anchor.shift(DOWN).shift(RIGHT).fill(RIGHT).is_not_blank().fill(DOWN).is_not_blank()
 
-    tab.col('A').is_not_blank.dimension(TIME, DIRECTLY, LEFT)
-    tab.filter("Agriculture, Forestry and Fishing").fill(LEFT).fill(RIGHT).is_not_blank.dimension('Broad Industry Group', CLOSEST, LEFT)
-    anchor.shift(RIGHT).shift(RIGHT).shift(UP).fill(LEFT).fill(RIGHT).is_not_blank.dimension('AWE Breakdown', DIRECTLY, ABOVE)
+    tab.col('A').is_not_blank().dimension(TIME, DIRECTLY, LEFT)
+    tab.filter("Agriculture, Forestry and Fishing").fill(LEFT).fill(RIGHT).is_not_blank().dimension('Broad Industry Group', CLOSEST, LEFT)
+    anchor.shift(RIGHT).shift(RIGHT).shift(UP).fill(LEFT).fill(RIGHT).is_not_blank().dimension('AWE Breakdown', DIRECTLY, ABOVE)
     return obs
