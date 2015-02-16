efa27f114d034cf844ff3a268508d140eace8fd3
Dragon Dave McKee <dragon@scraperwiki.com>
2015-02-16 10:44:27 +0000

    is_number as bag function

--- a02/a02.py
+++ a02/a02.py
@@ -4,7 +4,7 @@ def per_file(tableset):
     return "*"
 
 def per_tab(tab):
-    obs = tab.filter("MGSL").assert_one().shift(DOWN).fill(RIGHT).fill(DOWN).filter(is_number).is_not_italic
+    obs = tab.filter("MGSL").assert_one().shift(DOWN).fill(RIGHT).fill(DOWN).is_number().is_not_italic
 
     tab.col('A').one_of(['Male', 'Female', 'All Persons']).dimension('gender', CLOSEST, ABOVE)
     tab.col('A').is_date().dimension(TIME, DIRECTLY, LEFT)
