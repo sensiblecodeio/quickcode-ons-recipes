82c0ca6819574e7941fa152f6ad78c9bc9b071c9
Dragon Dave McKee <dragon@scraperwiki.com>
2015-02-19 13:43:03 +0000

    ons earn03.py

--- earn03/earn03.py
+++ earn03/earn03.py
@@ -4,10 +4,10 @@ def per_file(tableset):
     return "1. NSA ind monthly figs"
 
 def per_tab(tab):
-    anchor=tab.filter("CDID").assert_one()
-    obs = anchor.shift(DOWN).shift(RIGHT).fill(RIGHT).is_not_blank().fill(DOWN).is_not_blank()
+    obs = tab.filter("CDID").assert_one().fill(RIGHT).fill(DOWN).is_not_blank()
 
-    tab.col('A').is_not_blank().dimension(TIME, DIRECTLY, LEFT)
-    tab.filter("Agriculture, Forestry and Fishing").fill(LEFT).fill(RIGHT).is_not_blank().dimension('Broad Industry Group', CLOSEST, LEFT)
-    anchor.shift(RIGHT).shift(RIGHT).shift(UP).fill(LEFT).fill(RIGHT).is_not_blank().dimension('AWE Breakdown', DIRECTLY, ABOVE)
+    tab.col('A').dimension(TIME,DIRECTLY, LEFT)
+    tab.excel_ref("A3").fill(RIGHT).is_not_blank().dimension('Broad Industry Group', CLOSEST, LEFT)
+    tab.excel_ref("B6").expand(RIGHT).parent().is_not_blank().dimension('AWE Breakdown', DIRECTLY, ABOVE)
+
     return obs
