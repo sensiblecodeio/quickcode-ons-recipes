fafe3581c1ba45769bee33a8aa296f26879ad8b3
MikeData <Mikelovesbooks@gmail.com>
2016-10-06 13:20:29 +0100

    Added second estimate of GDP and multi factor productivity examples

---
+++ SecEstGDP/README.txt
@@ -0,0 +1,5 @@
+Something we tried (not a production process).
+Launch from the batch file. 
+Creates 14 datasets.
+Complicated.
+Post processing not included.
---
+++ SecEstGDP/bat.cmd
@@ -0,0 +1,15 @@
+
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "A1 AGGREGATES" None Y
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "A1 AGGREGATES" None Q
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "A2 AGGREGATES" None Y
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "A2 AGGREGATES" None Q
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "B1 CVM OUTPUT" "B2 CVM OUTPUT" Y
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "B1 CVM OUTPUT" "B2 CVM OUTPUT" Q
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "C1 EXPENDITURE" "C2 EXPENDITURE" Y
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "C1 EXPENDITURE" "C2 EXPENDITURE" Q
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "D INCOME" None Y
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "D INCOME" None Q
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "H1 TRADE" "H2 TRADE" Y
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "H1 TRADE" "H2 TRADE" Q
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "L GVAbp" None Y
+bake --preview recipe.py gdpq3m2reftables_tcm77-425649.xls "L GVAbp" None Q
