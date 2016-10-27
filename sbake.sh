ff63f15689fd9b03db8752943c67d8b14a9780ec
Steven Maude <git@stevenmaude.co.uk>
2016-10-27 12:44:34 +0100

    Try and generalise sbake.sh
    To maybe run on someone else's PC other than mine.

--- sbake.sh
+++ sbake.sh
@@ -6,5 +6,7 @@
     echo "databaker.databakersolo.runall(per_file, per_tab, '$1')"
 } > dbs.py
 
-PYTHONPATH=$PYTHONPATH:"/home/sm/.virtualenvs/databaker/src/databaker/" python dbs.py "${@:2}"
-
+full_bake_path=$(which bake)
+parent_bake_path=$(dirname "$full_bake_path")
+databaker_path=${parent_bake_path}/../src/databaker/
+PYTHONPATH=$PYTHONPATH:$databaker_path python dbs.py "${@:2}"
